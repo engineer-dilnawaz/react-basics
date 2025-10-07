1662ffb9cd08470d86450f37690c8a06c8350649
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-10-07 23:24:34 +0500

    add design changes and structure the home page

--- App.js
+++ App.js
@@ -1,40 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-  },
-  "Hello world from React!"
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://cdn.dribbble.com/userupload/16778067/file/original-d75cb39663149843b1572e4cc64681fe.jpg?resize=400x0"
+          alt="Foodie app"
+          className="logo"
+        />
+      </div>
 
-console.log(heading);
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello world from nested tags"),
-    React.createElement("h2", {}, "Hello world from  h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello world from nested tags"),
-    React.createElement("h2", {}, "Hello world from  h2"),
-  ]),
-]);
+const RestaurantCard = () => {
+  return (
+    <div className="rest-card">
+      <img
+        src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+        alt="food image"
+      />
+      <div className="details-container">
+        <div className="description">
+          <h3>Foodies Hub</h3>
+          <h4>Biryani, Indian, Asian</h4>
+        </div>
+        <div className="stats">
+          <h4>4.4 stars</h4>
+          <h4>38 minutes</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const jsxHeading = [
-  <h1 className="heading" tabIndex="3">
-    Hello World from JSX
-  </h1>,
-  <h1 className="heading" tabIndex="3">
-    Hello World from JSX
-  </h1>,
-];
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search bar</div>
+      <div className="rest-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-const Heading = () => {
-  return jsxHeading;
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<App />);
